7221104c01c96a48b175c5f22d51fc34d780e2ed
Tony <tursunoteshov@gmail.com>
2024-06-03 07:32:01 +0500

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3, 4, 5, };
+            int[] nums2 = { 1, 2, };
+            NumberOfPairs(nums1, nums2);
+        }
+
+        private static int NumberOfPairs(int[] nums1, int[] nums2)
+        {
+            return 1;
+        }
+
+       
+    }
+}
